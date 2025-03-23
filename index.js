7446bf6e4c2d9321cab89fe02c2964df08c8bac7
Victor <victormugo08@gmail.com>
2025-03-23 17:03:13 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,5 +1,74 @@
 /* Your Code Here */
 
+// Function to create an employee record
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateTimeString) {
+    if (!dateTimeString) {
+        throw new Error("Invalid dateTimeString input");
+    }
+    let [date, hour] = dateTimeString.split(" ");
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date,
+        hour: parseInt(hour, 10)
+    });
+    return this;
+}
+
+function createTimeOutEvent(dateTimeString) {
+    if (!dateTimeString) {
+        throw new Error("Invalid dateTimeString input");
+    }
+    let [date, hour] = dateTimeString.split(" ");
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date,
+        hour: parseInt(hour, 10)
+    });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find(e => e.date === date);
+    let timeOut = this.timeOutEvents.find(e => e.date === date);
+
+    if (!timeIn || !timeOut) {
+        throw new Error(`Missing timeIn or timeOut event for date: ${date}`);
+    }
+
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+function allWagesFor() {
+    return this.timeInEvents.reduce((total, e) => total + wagesEarnedOnDate.call(this, e.date), 0);
+}
+
+function findEmployeeByFirstName(collection, firstNameString) {
+    return collection.find(emp => emp.firstName === firstNameString);
+}
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((total, emp) => total + allWagesFor.call(emp), 0);
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -7,9 +76,8 @@
 
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
- */
-
-const allWagesFor = function () {
+ 
+ const AllWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -20,4 +88,4 @@ const allWagesFor = function () {
 
     return payable
 }
-
+*/ 
